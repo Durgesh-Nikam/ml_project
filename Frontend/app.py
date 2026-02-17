4a27687b9f84634e47916d641ccdfaac41274934
Durgesh-Nikam <98728473+Durgesh-Nikam@users.noreply.github.com>
2026-02-17 13:51:11 +0530

    Update app.py

--- Frontend/app.py
+++ Frontend/app.py
@@ -15,89 +15,89 @@ import tensorflow as tf
 BASE_DIR = os.path.dirname(__file__)
 
 
-# MODEL_PATH = os.path.join(
-#     BASE_DIR,
-#     "Models",
-#     "Disease",
-#     "plant_disease_model.keras"
-# )
-
 MODEL_PATH = os.path.join(
     BASE_DIR,
     "Models",
-    "Disease"
+    "Disease",
+    "plant_disease_model.keras"
 )
+
+# MODEL_PATH = os.path.join(
+#     BASE_DIR,
+#     "Models",
+#     "Disease"
+# )
 
 
 # # -------- Disease Model Loading --------
-# @st.cache_resource
-# def load_cnn_model():
-#     try:
-#         model = load_model(MODEL_PATH, compile=False)
-
-#         with open(os.path.join(BASE_DIR, "Models", "Disease", "class_indices.json")) as f:
-#             class_indices = json.load(f)
-
-#         inv_class_indices = {v: k for k, v in class_indices.items()}
-#         return model, inv_class_indices
-
-#     except Exception as e:
-#         st.error("❌ ERROR LOADING CNN MODEL")
-#         st.exception(e)
-#         return None, None
 @st.cache_resource
 def load_cnn_model():
     try:
-        model = tf.keras.models.load_model(MODEL_PATH)
+        model = load_model(MODEL_PATH, compile=False)
 
-        # Manual class names (PlantVillage 38 classes)
-        class_names = [
-            'Apple___Apple_scab',
-            'Apple___Black_rot',
-            'Apple___Cedar_apple_rust',
-            'Apple___healthy',
-            'Blueberry___healthy',
-            'Cherry_(including_sour)___healthy',
-            'Cherry_(including_sour)___Powdery_mildew',
-            'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
-            'Corn_(maize)___Common_rust_',
-            'Corn_(maize)___healthy',
-            'Corn_(maize)___Northern_Leaf_Blight',
-            'Grape___Black_rot',
-            'Grape___Esca_(Black_Measles)',
-            'Grape___healthy',
-            'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
-            'Orange___Haunglongbing_(Citrus_greening)',
-            'Peach___Bacterial_spot',
-            'Peach___healthy',
-            'Pepper,_bell___Bacterial_spot',
-            'Pepper,_bell___healthy',
-            'Potato___Early_blight',
-            'Potato___healthy',
-            'Potato___Late_blight',
-            'Raspberry___healthy',
-            'Soybean___healthy',
-            'Squash___Powdery_mildew',
-            'Strawberry___healthy',
-            'Strawberry___Leaf_scorch',
-            'Tomato___Bacterial_spot',
-            'Tomato___Early_blight',
-            'Tomato___healthy',
-            'Tomato___Late_blight',
-            'Tomato___Leaf_Mold',
-            'Tomato___Septoria_leaf_spot',
-            'Tomato___Spider_mites Two-spotted_spider_mite',
-            'Tomato___Target_Spot',
-            'Tomato___Tomato_mosaic_virus',
-            'Tomato___Tomato_Yellow_Leaf_Curl_Virus'
-        ]
+        with open(os.path.join(BASE_DIR, "Models", "Disease", "class_indices.json")) as f:
+            class_indices = json.load(f)
 
-        return model, class_names
+        inv_class_indices = {v: k for k, v in class_indices.items()}
+        return model, inv_class_indices
 
     except Exception as e:
         st.error("❌ ERROR LOADING CNN MODEL")
         st.exception(e)
         return None, None
+# @st.cache_resource
+# def load_cnn_model():
+#     try:
+#         model = tf.keras.models.load_model(MODEL_PATH)
+
+#         # Manual class names (PlantVillage 38 classes)
+#         class_names = [
+#             'Apple___Apple_scab',
+#             'Apple___Black_rot',
+#             'Apple___Cedar_apple_rust',
+#             'Apple___healthy',
+#             'Blueberry___healthy',
+#             'Cherry_(including_sour)___healthy',
+#             'Cherry_(including_sour)___Powdery_mildew',
+#             'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+#             'Corn_(maize)___Common_rust_',
+#             'Corn_(maize)___healthy',
+#             'Corn_(maize)___Northern_Leaf_Blight',
+#             'Grape___Black_rot',
+#             'Grape___Esca_(Black_Measles)',
+#             'Grape___healthy',
+#             'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+#             'Orange___Haunglongbing_(Citrus_greening)',
+#             'Peach___Bacterial_spot',
+#             'Peach___healthy',
+#             'Pepper,_bell___Bacterial_spot',
+#             'Pepper,_bell___healthy',
+#             'Potato___Early_blight',
+#             'Potato___healthy',
+#             'Potato___Late_blight',
+#             'Raspberry___healthy',
+#             'Soybean___healthy',
+#             'Squash___Powdery_mildew',
+#             'Strawberry___healthy',
+#             'Strawberry___Leaf_scorch',
+#             'Tomato___Bacterial_spot',
+#             'Tomato___Early_blight',
+#             'Tomato___healthy',
+#             'Tomato___Late_blight',
+#             'Tomato___Leaf_Mold',
+#             'Tomato___Septoria_leaf_spot',
+#             'Tomato___Spider_mites Two-spotted_spider_mite',
+#             'Tomato___Target_Spot',
+#             'Tomato___Tomato_mosaic_virus',
+#             'Tomato___Tomato_Yellow_Leaf_Curl_Virus'
+#         ]
+
+#         return model, class_names
+
+#     except Exception as e:
+#         st.error("❌ ERROR LOADING CNN MODEL")
+#         st.exception(e)
+#         return None, None
 
 
 
